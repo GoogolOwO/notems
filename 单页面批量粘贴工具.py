311483c4ecdb0ed19d2c288eaf99825212e4f133
Googol <googology+github@outlook.com>
2024-07-17 11:20:28 +0800

    更符合实际意义

--- 单页面批量粘贴工具.py
+++ 单页面批量粘贴工具.py
@@ -1,7 +1,7 @@
 # 引入selenium库中的 webdriver 模块
 from selenium import webdriver
 print("Note.ms 批量写入内容工具")
-print("请勿用于批量打广告等违规操作！对此带来的一切后果本工具开发者概不负责！请确保您已安装Chrome且为最新版本，driver.exe在同一目录下")
+print("请勿用于批量打广告等操作，note.ms 没有规则。但因为违法对此带来的一切后果本工具开发者概不负责！请确保您已安装Chrome且为最新版本，driver.exe在同一目录下")
 page = input("请输入想要显示的页面，请确保没有文本！（示例：如欲在note.ms/test显示，请输入test\n")
 text = input("请输入想要显示的文本（换行请写\\n）\n")
 times = int(input("请输入次数"))
